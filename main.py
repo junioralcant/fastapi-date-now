08d276ae47aaaf048113ffcde21ce5ff5c61133b
junioralcant <junioralcant111@gmail.com>
2022-07-15 09:37:30 -0300

    Formatted date timezone São Paulo

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from distutils.log import debug
 from fastapi import FastAPI
 from datetime import datetime
+from pytz import timezone
 
 app = FastAPI()
 
-dateNow = datetime.now();
+dateNow = datetime.now()
 
-dateFormatted = dateNow.strftime("%d/%m/%Y %H:%M:%S")
+dateBrasil = timezone("America/Sao_Paulo")
+
+date = dateNow.astimezone(dateBrasil)
+
+dateFormatted = date.strftime("%d/%m/%Y %H:%M:%S")
+
 
 @app.get('/')
 def home():
-    return {"date": dateFormatted} 
+    return {"date": dateFormatted}
 
-if __name__ == "__main__": 
-    app.run(debug=True)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
