179c91cc6e6cbd327a6f9de6131ab8c2e965350c
junioralcant <junioralcant111@gmail.com>
2022-07-15 09:51:07 -0300

    Resolved bug list date now

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from distutils.log import debug
 from fastapi import FastAPI
-from datetime import datetime
+from datetime import date, datetime
 from pytz import timezone
 
 app = FastAPI()
 
-dateNow = datetime.now()
 
-dateBrasil = timezone("America/Sao_Paulo")
+def now():
 
-date = dateNow.astimezone(dateBrasil)
+    dateNow = datetime.now()
 
-dateFormatted = date.strftime("%d/%m/%Y %H:%M:%S")
+    dateBrasil = timezone("America/Sao_Paulo")
+
+    date = dateNow.astimezone(dateBrasil)
+
+    dateFormatted = date.strftime("%d/%m/%Y %H:%M:%S")
+
+    return dateFormatted
 
 
 @app.get('/')
 def home():
-    return {"date": dateFormatted}
+    return {"date": now()}
 
 
 if __name__ == "__main__":
